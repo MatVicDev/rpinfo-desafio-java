9e66d34355f82a49e31a68ca12b71b51a8f53c46
Matheus Victor <matheusvic013@gmail.com>
2024-08-02 19:34:51 -0300

    Iniciando projeto de controle de manutenção de equipamentos

---
+++ src/main/java/br/com/rpinfo/rp_desafio_java/RpDesafioJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpinfo.rp_desafio_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpDesafioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpDesafioJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rpinfo/rp_desafio_java/RpDesafioJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rpinfo.rp_desafio_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpDesafioJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
