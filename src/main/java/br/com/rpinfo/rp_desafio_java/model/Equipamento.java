0b036bfd4fdf46a3e5c38c69a8270d90c6e04465
Matheus Victor <matheusvic013@gmail.com>
2024-08-03 12:44:24 -0300

    Modelando classe de Equipamento

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/Equipamento.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Equipamento.java
@@ -1,4 +1,9 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
 public class Equipamento {
+    private Long id;
+    private String tipo;
+    private String marca;
+    private String descricao;
+    private Cliente cliente;
 }
