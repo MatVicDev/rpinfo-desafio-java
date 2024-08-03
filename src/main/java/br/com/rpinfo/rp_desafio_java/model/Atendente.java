5126e88f3aceae6a0691dabbe13978457038e536
Matheus Victor <matheusvic013@gmail.com>
2024-08-03 17:05:44 -0300

    Modelando a classe Atendente

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/Atendente.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Atendente.java
@@ -1,4 +1,14 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
 public class Atendente {
+    private Long id;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome() {
+        this.nome = "Beatriz";
+    }
 }
