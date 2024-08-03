4c42d850be83e55aca7b3342ea35c745af14e03a
Matheus Victor <matheusvic013@gmail.com>
2024-08-03 17:21:55 -0300

    Criando construtor para a classe Cliente

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/Cliente.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Cliente.java
@@ -3,9 +3,16 @@ package br.com.rpinfo.rp_desafio_java.model;
 public class Cliente {
     private Long id;
     private String nome;
-    private String endereco;
+    private Endereco endereco;
     private String telefone;
     private String email;
     private Equipamento equipamento;
     private OrdemServico ordemServico;
+
+    public Cliente(String nome, Endereco endereco, String telefone, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
 }
