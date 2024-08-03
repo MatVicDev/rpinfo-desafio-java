4079f5a624393ebe98d8bc4322b41ecec2c93fa7
Matheus Victor <matheusvic013@gmail.com>
2024-08-03 17:06:38 -0300

    Modelando a classe Tecnico

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/Tecnico.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Tecnico.java
@@ -1,4 +1,14 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
 public class Tecnico {
+    private Long Id;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome() {
+        this.nome = "Rodrigo";
+    }
 }
