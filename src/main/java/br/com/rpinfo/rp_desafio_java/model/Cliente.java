2240c7aa8a014d76304a45a7449070935b5340e3
Matheus Victor <matheusvic013@gmail.com>
2024-08-03 12:28:39 -0300

    Modelando a classe Cliente

---
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Cliente.java
@@ -0,0 +1,11 @@
+package br.com.rpinfo.rp_desafio_java.model;
+
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String email;
+    private Equipamento equipamento;
+    private OrdemServico ordemServico;
+}
