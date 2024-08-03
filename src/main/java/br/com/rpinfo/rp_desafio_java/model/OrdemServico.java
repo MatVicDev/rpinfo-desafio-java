e86e2a75af853f9f0df28a1fef42b2192169fc97
Matheus Victor <matheusvic013@gmail.com>
2024-08-03 17:08:21 -0300

    Criando método para formatar data

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/OrdemServico.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/OrdemServico.java
@@ -1,11 +1,19 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class OrdemServico {
     private Long id;
     private Atendente atendente;
     private Tecnico tecnico;
-    private LocalDateTime dataEntrada;
+    private LocalDateTime dataEntrada = LocalDateTime.now();
     private String descricao;
+
+    public String getDataFormata() {
+        var formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm");
+        var dataFormatada = this.dataEntrada.format(formatador);
+
+        return dataFormatada;
+    }
 }
