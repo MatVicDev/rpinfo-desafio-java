d45eb3bdae7715f1f28cda0fbd4361864f7ae8da
Matheus Victor <matheusvic013@gmail.com>
2024-08-05 10:50:55 -0300

    Relacionando tabelas

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/OrdemServico.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/OrdemServico.java
@@ -1,17 +1,25 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Entity
 public class OrdemServico {
+
+    @Id
     private Long id;
-    private Atendente atendente;
-    private Tecnico tecnico;
+    private String atendente = "Beatriz";
+    private String tecnico = "Rodrigo";
     private LocalDateTime dataEntrada = LocalDateTime.now();
     private String descricao;
 
     private Status status;
 
+    @OneToOne
     private Equipamento equipamento;
 
     public OrdemServico(String descricao, Equipamento equipamento) {
