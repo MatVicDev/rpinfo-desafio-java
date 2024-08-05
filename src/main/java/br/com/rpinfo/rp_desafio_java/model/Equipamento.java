6e9e20628a0eeff33eeab24920724a9b936c1115
Matheus Victor <matheusvic013@gmail.com>
2024-08-05 15:28:10 -0300

    Relacionado tabelas

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/Equipamento.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Equipamento.java
@@ -1,20 +1,30 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Equipamento {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String tipo;
     private String marca;
     private String problema;
 
-    @OneToOne
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    @OneToOne
+    @JoinColumn(name = "ordem_servico_id")
+    private OrdemServico ordemServico;
+
+    public Equipamento() {
+
+    }
 
     public Equipamento(String tipo, String marca, String problema, Cliente cliente) {
         this.tipo = tipo;
