db458cdb998dbc5af314a4886ba2398b6e3a9cec
Matheus Victor <matheusvic013@gmail.com>
2024-08-05 10:30:45 -0300

    Adicionando o @Getter e relacionando as tabelas

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/Cliente.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Cliente.java
@@ -1,13 +1,31 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @ManyToOne
     private Endereco endereco;
     private String telefone;
     private String email;
-    private Equipamento equipamento;
+    @OneToMany
+    private List<Equipamento> equipamentos;
+
+    @OneToOne
     private OrdemServico ordemServico;
+
+    public Cliente() {
+    }
 
     public Cliente(String nome, Endereco endereco, String telefone, String email) {
         this.nome = nome;
