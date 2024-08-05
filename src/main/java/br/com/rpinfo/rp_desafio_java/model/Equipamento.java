8662dfce4fd6140aefc3dc528e5f509491f6a5a6
Matheus Victor <matheusvic013@gmail.com>
2024-08-05 10:36:56 -0300

    Relacionado as tabelas

--- src/main/java/br/com/rpinfo/rp_desafio_java/model/Equipamento.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/model/Equipamento.java
@@ -1,10 +1,19 @@
 package br.com.rpinfo.rp_desafio_java.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Equipamento {
+
+    @Id
     private Long id;
     private String tipo;
     private String marca;
     private String problema;
+
+    @OneToOne
     private Cliente cliente;
 
     public Equipamento(String tipo, String marca, String problema, Cliente cliente) {
