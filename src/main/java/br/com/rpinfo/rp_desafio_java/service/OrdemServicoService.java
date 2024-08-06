f8e68b14a5cf402285113a014e23884f172447c4
Matheus Victor <matheusvic013@gmail.com>
2024-08-06 11:40:27 -0300

    Criando Service para ordens de serviços

---
+++ src/main/java/br/com/rpinfo/rp_desafio_java/service/OrdemServicoService.java
@@ -0,0 +1,38 @@
+package br.com.rpinfo.rp_desafio_java.service;
+
+import br.com.rpinfo.rp_desafio_java.model.Equipamento;
+import br.com.rpinfo.rp_desafio_java.model.OrdemServico;
+import br.com.rpinfo.rp_desafio_java.repository.ClienteRepository;
+import br.com.rpinfo.rp_desafio_java.repository.EquipamentoRepository;
+import br.com.rpinfo.rp_desafio_java.repository.OrdemServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrdemServicoService {
+
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EquipamentoRepository equipamentoRepository;
+
+
+    public OrdemServico cadastrarOrdemServico(String descricao, Long equipamentoId) {
+        Equipamento equipamento = equipamentoRepository.findById(equipamentoId).orElseThrow(() -> new IllegalArgumentException("Cliente não cadastrado!"));
+
+        OrdemServico ordemServico = new OrdemServico(descricao, equipamento);
+        ordemServico.setCliente(equipamento.getCliente());
+
+        equipamento.setOrdemServico(ordemServico);
+
+        return ordemServicoRepository.save(ordemServico);
+    }
+
+    public OrdemServico consultarOrdemServico(Long id) {
+        return ordemServicoRepository.findById(id).orElse(null);
+    }
+}
