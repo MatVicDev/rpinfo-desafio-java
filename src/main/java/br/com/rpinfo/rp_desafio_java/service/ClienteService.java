d0e71208b9fcb0e8569c3de7ba6f3538f23652eb
Matheus Victor <matheusvic013@gmail.com>
2024-08-06 11:39:10 -0300

    Adicionando Exceção caso não encontre o cliente

--- src/main/java/br/com/rpinfo/rp_desafio_java/service/ClienteService.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/service/ClienteService.java
@@ -3,6 +3,7 @@ package br.com.rpinfo.rp_desafio_java.service;
 import br.com.rpinfo.rp_desafio_java.model.Cliente;
 import br.com.rpinfo.rp_desafio_java.model.Endereco;
 import br.com.rpinfo.rp_desafio_java.repository.ClienteRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +13,13 @@ public class ClienteService {
     @Autowired
     private ClienteRepository repository;
 
+    @Transactional
     public Cliente cadastrarCliente(String nome, Endereco endereco, String telefone, String email) {
         Cliente cliente = new Cliente(nome, endereco, telefone, email);
         return repository.save(cliente);
     }
 
     public Cliente consultarCliente(Long id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado"));
     }
 }
