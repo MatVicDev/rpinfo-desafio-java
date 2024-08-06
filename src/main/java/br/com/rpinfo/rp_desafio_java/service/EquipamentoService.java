e3a1e3d502ce1d7fee8fec5fbd4da398bf2fe8cb
Matheus Victor <matheusvic013@gmail.com>
2024-08-06 11:39:53 -0300

    Criando Service para equipamentos

---
+++ src/main/java/br/com/rpinfo/rp_desafio_java/service/EquipamentoService.java
@@ -0,0 +1,31 @@
+package br.com.rpinfo.rp_desafio_java.service;
+
+import br.com.rpinfo.rp_desafio_java.model.Cliente;
+import br.com.rpinfo.rp_desafio_java.model.Equipamento;
+import br.com.rpinfo.rp_desafio_java.repository.ClienteRepository;
+import br.com.rpinfo.rp_desafio_java.repository.EquipamentoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EquipamentoService {
+
+    @Autowired
+    private EquipamentoRepository equipamentoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Transactional
+    public Equipamento cadastrarEquipamento(String tipo, String marca, String problema, Long clienteId) {
+        Cliente cliente = clienteRepository.findById(clienteId).orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado"));
+
+        Equipamento equipamento = new Equipamento(tipo, marca, problema, cliente);
+        return equipamentoRepository.save(equipamento);
+    }
+
+    public Equipamento consultarEquipamento(Long id) {
+        return equipamentoRepository.findById(id).orElse(null);
+    }
+}
