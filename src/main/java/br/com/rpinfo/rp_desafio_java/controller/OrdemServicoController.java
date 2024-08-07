8c9ddd3ad473e1190690808e81932b48cc981149
Matheus Victor <matheusvic013@gmail.com>
2024-08-07 10:10:49 -0300

    Retornando a lista de ordens de serviço por API

--- src/main/java/br/com/rpinfo/rp_desafio_java/controller/OrdemServicoController.java
+++ src/main/java/br/com/rpinfo/rp_desafio_java/controller/OrdemServicoController.java
@@ -2,6 +2,7 @@ package br.com.rpinfo.rp_desafio_java.controller;
 
 import br.com.rpinfo.rp_desafio_java.model.OrdemServico;
 import br.com.rpinfo.rp_desafio_java.repository.OrdemServicoRepository;
+import br.com.rpinfo.rp_desafio_java.service.OrdemServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +10,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping(value = "/ordem_servico")
 public class OrdemServicoController {
 
     @Autowired
-    private OrdemServicoRepository repository;
+    private OrdemServicoService ordemServicoService;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<OrdemServico> consultaPorId(@PathVariable Long id) {
-        return ResponseEntity.ok(repository.findById(id).get());
+        OrdemServico ordemServico = ordemServicoService.consultarOrdemServico(id).get();
+        return ResponseEntity.ok(ordemServico);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrdemServico>> listarOrdensServico() {
+        return ResponseEntity.ok(ordemServicoService.listarOrdensServico());
     }
 }
